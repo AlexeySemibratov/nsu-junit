1067fd42bcb0f432581a97d34acb70af7aeda035
Alexey Semibratov <jaxel1999@gmail.com>
2021-05-08 16:23:33 +0700

    - many fixes 2

--- JUnit/src/org/nsu/junit/assertions/AssertionException.java
+++ JUnit/src/org/nsu/junit/assertions/AssertionException.java
@@ -2,7 +2,7 @@ package org.nsu.junit.assertions;
 
 public class AssertionException extends RuntimeException {
 
-	public AssertionException(String message) {
-		super(message);
-	}
+    public AssertionException(String message) {
+        super(message);
+    }
 }
